bdf8581b22387e955e9e2202b9985146d04222bb
yuzo <yuzokamoto@gmail.com>
2021-08-24 18:06:21 -0300

    fix

--- pokedex/src/components/Detalhes.js
+++ pokedex/src/components/Detalhes.js
@@ -1,7 +1,28 @@
 import React from "react"
+import { Header } from "./Home"
+import { useHistory } from "react-router-dom"
+import { ContainerButton } from "./Home"
 
-export const Detalhes =() =>{
-    return(
-        <div>Detalhes</div>
+export const Detalhes = () => {
+
+    const history = useHistory()
+
+    const irParaInicio = () => {
+        history.goBack("/")
+    }
+
+    const irParaPokedex = () => {
+        history.push("/pokedex")
+    }
+    return (
+        <div>
+            <Header>
+                <h2>Detalhes</h2>
+            </Header>
+            <ContainerButton>
+                <button onClick={irParaInicio}>Voltar</button>
+                <button onClick={irParaPokedex}>Ir para pokedex</button>
+            </ContainerButton>
+        </div>
     )
 }
