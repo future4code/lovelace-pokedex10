3d7ec469c402151b6db2946bb143d19483fb6347
camilagsoares <camila.soares19@hotmail.com>
2021-08-25 16:28:11 -0300

    Subindo as mudanças com grid e a lista de pokemons

--- pokedex/src/components/Detalhes.js
+++ pokedex/src/components/Detalhes.js
@@ -1,7 +1,19 @@
 import React from "react"
 import { Header } from "./Home"
 import { useHistory } from "react-router-dom"
-import { ContainerButton } from "./Home"
+import { ContainerButtonHeader } from "./Home"
+import styled from "styled-components"
+
+const CardDetalhesLateral = styled.div`
+display: grid;
+/* grid-template-columns: 100px 100px; */
+
+div {
+   margin: 13px;
+   background-color: white;
+   width: 230px;
+   height: 200px;
+   box-shadow: 1px 0px 4px #BAB9C7;} `
 
 export const Detalhes = () => {
 
@@ -19,10 +31,15 @@ export const Detalhes = () => {
             <Header>
                 <h2>Detalhes</h2>
             </Header>
-            <ContainerButton>
+            <ContainerButtonHeader>
                 <button onClick={irParaInicio}>Voltar</button>
                 <button onClick={irParaPokedex}>Ir para pokedex</button>
-            </ContainerButton>
+            </ContainerButtonHeader>
+        
+            <CardDetalhesLateral> <div>Foto 1</div> </CardDetalhesLateral>
+            <CardDetalhesLateral> <div>Foto 2</div> </CardDetalhesLateral>
+           
+
         </div>
     )
 }
